e0b9d5493a8ac500c7f33885d29b9c07fbddf006
Supriyam-Mishra <supriyammishra1@gmail.com>
2026-03-07 14:53:37 +0530

    Dir setup

---
+++ template.sh
@@ -0,0 +1,16 @@
+# Creating directories
+mkdir -p src
+mkdir -p reseource
+
+# creating files
+touch src/__init__.py
+touch src/helper.py
+touch src/prompt.py
+touch .env
+touch setup.py
+touch app.py
+touch reseaource/trials.ipynb
+touch requirements.txt
+touch README.md
+
+echo "Directories and files created successfully!"
