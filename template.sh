19387b360edecb94039797df743158e2bb90baa4
Supriyam-Mishra <supriyammishra1@gmail.com>
2026-03-07 15:00:18 +0530

    dir structure

--- template.sh
+++ template.sh
@@ -1,6 +1,6 @@
 # Creating directories
 mkdir -p src
-mkdir -p reseource
+mkdir -p research
 
 # creating files
 touch src/__init__.py
@@ -9,7 +9,7 @@ touch src/prompt.py
 touch .env
 touch setup.py
 touch app.py
-touch reseaource/trials.ipynb
+touch research/trials.ipynb
 touch requirements.txt
 touch README.md
 
